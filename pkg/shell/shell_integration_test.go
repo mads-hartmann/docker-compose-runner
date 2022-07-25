9efbc9aab2a7e65b9d2b17bf248a0a4ff98eb633
Mads Hartmann <mads379@gmail.com>
2022-07-25 14:44:25 +0000

    Switch to table tests

--- pkg/shell/shell_integration_test.go
+++ pkg/shell/shell_integration_test.go
@@ -8,14 +8,38 @@ import (
 )
 
 func TestCommand(t *testing.T) {
-	stdout, stderr, err := Command("file testdata/example.md")
-	if err != nil {
-		t.Error("Expected no error, got", err)
+
+	data := []struct {
+		name    string
+		command string
+		stdout  string
+		stderr  string
+		errMsg  string
+	}{
+		{"Filesystem access", "file testdata/example.md", "testdata/example.md: ASCII text", "", ""},
+		{"Echo stdout", "echo Hello to stdout", "Hello to stdout", "", ""},
+		{"Echo stderr", ">&2 echo Hello to stderr", "", "Hello to stderr", ""},
 	}
-	if stderr != "" {
-		t.Error("Expected empty stderr, got", stderr)
-	}
-	if strings.TrimSpace(stdout) != "testdata/example.md: ASCII text" {
-		t.Error("Expected foo got", stdout)
+
+	for _, d := range data {
+		t.Run(d.name, func(t *testing.T) {
+			stdout, stderr, err := Command(d.command)
+
+			if strings.TrimSpace(stdout) != d.stdout {
+				t.Errorf("Expected stdout of '%s' got '%s'", d.stdout, stdout)
+			}
+
+			if strings.TrimSpace(stderr) != d.stderr {
+				t.Errorf("Expected stderr of '%s' got '%s'", d.stderr, stderr)
+			}
+
+			var errMsg string
+			if err != nil {
+				errMsg = err.Error()
+			}
+			if strings.TrimSpace(errMsg) != d.errMsg {
+				t.Errorf("Expected error message '%s', got '%s'", d.errMsg, errMsg)
+			}
+		})
 	}
 }
